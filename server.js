fedfe2426dc9a0f8846bb310f346d01da1d72bf0
Chris Jones <chrisjonesdesign85@gmail.com>
2019-07-25 23:47:12 -0400

    got through the guided project, no errors now!

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const hubsRouter = require('./hubs/hubs-router');
+
+const server = express();
+
+server.use(express.json());
+server.use('./api/hubs', hubsRouter);
+// server.use('/api/message', messageRouter;
+// server.use('./api/users', usersRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+module.exports = server;
